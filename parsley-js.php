6594d235b782915073d1c815d009462f76874289
Sukanta Roy Sujan <sujan7a@gmail.com>
2018-11-26 13:49:19 +0600

    parsley php function
    with correct argument it will return everything you need in your input field

---
+++ parsley-js.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Documentation
+http://parsleyjs.org/doc/index.html
+*/
+
+function parsley( $required, $type = 'text', $minlength = 0, $maxlength = 0 ) {
+	$return = '';
+	//required or not
+	if ( $required == 'true' ) {
+		$return = $return . 'data-parsley-required="true"'; // required and validate
+		//$return=$return.'data-parsley-required';	// required and validate
+	} else {
+		$return = $return . 'data-parsley-required="false"'; // not required but validate
+	}
+	// type
+	if ( $type == 'text' ) {
+		//$return=$return.' data-parsley-type="text"';	
+	} elseif ( $type == 'email' ) {
+		$return = $return . ' data-parsley-type="email"';
+	} elseif ( $type == 'url' ) {
+		$return = $return . ' data-parsley-type="url"';
+	} elseif ( $type == 'integer' ) {
+		$return = $return . ' data-parsley-type="integer"';
+	} elseif ( $type == 'digits' ) {
+			$return = $return . ' data-parsley-type="digits"';
+		}
+		// minlength maxlength
+	if ( $minlength != 0 && $maxlength == 0 && $type != 'check' ) {
+		// only minlength
+		$return = $return . ' data-parsley-minlength="' . $minlength . '"';
+	} elseif ( $minlength == 0 && $maxlength != 0 && $type != 'check' ) {
+		// only maxlength
+		$return = $return . ' data-parsley-maxlength="' . $minlength . '"';
+	} elseif ( $minlength != 0 && $maxlength != 0 && $type != 'check' ) {
+		// only maxlength
+		$return = $return . ' data-parsley-length="[' . $minlength . ', ' . $maxlength . ']"';
+	} elseif ( $minlength != 0 && $maxlength == 0 && $type == 'check' ) {
+		// only minlength
+		$return = $return . ' data-parsley-mincheck="' . $minlength . '"';
+	} elseif ( $minlength == 0 && $maxlength != 0 && $type == 'check' ) {
+		// only maxlength
+		$return = $return . ' data-parsley-maxcheck="' . $minlength . '"';
+	} elseif ( $minlength != 0 && $maxlength != 0 && $type == 'check' ) {
+		// only maxlength
+		$return = $return . ' data-parsley-check="[' . $minlength . ', ' . $maxlength . ']"';
+	}
+	echo $return;
+}
+?>
